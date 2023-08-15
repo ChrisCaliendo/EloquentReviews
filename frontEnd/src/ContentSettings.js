5051b52db11f5ccc782b985bebb4787973080b67
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-08-15 18:04:04 -0400

    not important

--- frontEnd/src/ContentSettings.js
+++ frontEnd/src/ContentSettings.js
@@ -8,18 +8,18 @@ const ContentSettings = () => {
         <div>
             
 
-            <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Identification</h3>
+            <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Select Which Games Do You Want Reviews From</h3>
             <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                     <div class="flex items-center pl-3">
                         <input id="horizontal-list-radio-license" type="radio" value="topSelling" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                        <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Driver License </label>
+                        <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Top Selling</label>
                     </div>
                 </li>
                 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                     <div class="flex items-center pl-3">
                         <input id="horizontal-list-radio-id" type="radio" value="mostPlayed" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                        <label for="horizontal-list-radio-id" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">State ID</label>
+                        <label for="horizontal-list-radio-id" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Most Played</label>
                     </div>
                 </li>
                 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
@@ -42,7 +42,7 @@ const ContentSettings = () => {
 
             
             <form method="POST" action="/settings">
-                <button type="submit"> Apply Setting Changes</button>
+                <button type="submit"> Apply Setting </button>
             </form>
         </div>
     );
