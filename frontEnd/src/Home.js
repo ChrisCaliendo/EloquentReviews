00e3ba52abfd2b8ff9025ed0f2fd088a6276017a
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-09-11 12:44:46 -0400

    Update Home.js

--- frontEnd/src/Home.js
+++ frontEnd/src/Home.js
@@ -99,7 +99,7 @@ const Home = () => {
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </div>
-                        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-600 focus:border-gray-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-400 " placeholder="Enter Name of Game" required/>
+                        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-600 focus:border-gray-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-400 " placeholder="Enter Name of Game  (( WIP ))" required/>
                         <br />                
                     </div>
                     <div className="flex justify-evenly">
