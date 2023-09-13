f972428c4be3705dccbd1cc409075da9fd276c41
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-09-13 16:08:04 -0400

    trying to make things work

--- frontEnd/src/ContentSettings.js
+++ frontEnd/src/ContentSettings.js
@@ -14,21 +14,18 @@ const ContentSettings = () => {
     const [lengthType, setLengthType] = useState(contentConfig.lengthType)
 
     const applyContentSettings = () => {
+        console.log("yep")
         setContextConfig({
             useConfig: useConfig,
-            gameType: gameType,
+            gameTags: gameTags,
+            reviewRating: reviewRating,
             reviewLength: reviewLength,
-            lengthType: lengthType,
-            reviewRating: reviewRating
+            lengthType: lengthType
         })
     };
 
     const handleSliderChange = (event) => {
         setReviewLimit(event.target.value);
-    };
-
-    const handleTypeChange = (event) => {
-        setGameType(event.target.value);
     };
 
     const handleRatingChange = (event) => {
@@ -37,38 +34,36 @@ const ContentSettings = () => {
 
     const handleToggle = (event) => {
         setUseConfig(!event.target.value);
-        console.log(useConfig)
     };
-
-    
 
     const handleCheckboxChange = (value, isChecked) => {
         if (isChecked) {
-            setSelectedValues([...selectedValues, value]);
+            setGameTags([...gameTags, value]);
         } 
         else {
-            etSelectedValues(gameTags.filter(item => item !== value));
+            setGameTags(gameTags.filter(item => item !== value));
         }
+        console.log(gameTags)
     };
 
     return (  
         
         <div className="border border-none shadow rounded-md p-5 max-w-3xl w-full mx-auto px-3 py-2 text-white bg-zinc-900 font-semibold">
             <h3 className="mb-4 font-semibold dark:text-white">What Kind of Games Do You Want To Find?</h3>
-            <div onChange={handleTypeChange} className="grid grid-cols-4 gap-4">
+            <div  className="grid grid-cols-4 gap-4">
                 
-                <SteamTag onChange={handleCheckboxChange} tagName="Indie" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Adventure" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Singleplayer" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Multiplayer" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Action" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Strategy" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Casual" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Rougelike" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Simulation" />
-                <SteamTag onChange={handleCheckboxChange} tagName="FPS" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Puzzle" />
-                <SteamTag onChange={handleCheckboxChange} tagName="Metroidvania" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Indie" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Adventure" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Singleplayer" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Multiplayer" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Action" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Strategy" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Casual" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Rougelike" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Simulation" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="FPS" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Puzzle" />
+                <SteamTag onChange={handleCheckboxChange} checkCondition={ gameTags } tagName="Metroidvania" />
                 
             </div>
             <br />
@@ -78,20 +73,20 @@ const ContentSettings = () => {
                 <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-xl sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                     <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 accent-gray-900">
                         <div className="flex items-center pl-3">
-                            <input id="horizontal-list-radio-license" type="radio" value="Any" name="rating-input" checked={reviewRating === "Any"} className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                            <label for="horizontal-list-radio-license" className="w-full py-3 ml-2 text-sm font-bold text-gray-900 dark:text-gray-300">Any</label>
+                            <input id="horizontal-list-radio-license" type="radio" value="Any" name="rating-input" defaultChecked={ reviewRating === "Any" } className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                            <label className="w-full py-3 ml-2 text-sm font-bold text-gray-900 dark:text-gray-300">Any</label>
                         </div>
                     </li>
                     <li className="w-full border-b border-gray-200 bg-blue-300 sm:border-b-0 sm:border-r dark:border-gray-600">
                         <div className="flex items-center pl-3">
-                            <input id="horizontal-list-radio-id" type="radio" value="Positive" name="rating-input" checked={reviewRating === "Positive"} className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 accent-gray-900 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                            <label for="horizontal-list-radio-id" className="w-full py-3 ml-2 text-sm font-bold text-gray-900  ">Postive</label>
+                            <input id="horizontal-list-radio-id" type="radio" value="Positive" name="rating-input" defaultChecked={ reviewRating === "Positive" } className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 accent-gray-900 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                            <label className="w-full py-3 ml-2 text-sm font-bold text-gray-900  ">Postive</label>
                         </div>
                     </li>
                     <li className="w-full rounded-r-xl border-b  border-gray-200 bg-red-300 sm:border-b-0 sm:border-r dark:border-gray-600">
                         <div className="flex items-center pl-3 ">
-                            <input id="horizontal-list-radio-millitary" type="radio" value="Negative" name="rating-input" checked={reviewRating === "Negative"} className="w-4 h-4 text-gray-600 bg-gray-100  border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 accent-gray-900 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                            <label for="horizontal-list-radio-millitary" className="w-full py-3 ml-2 text-sm font-bold text-gray-900 accent-gray-800 ">Negative</label>
+                            <input id="horizontal-list-radio-millitary" type="radio" value="Negative" name="rating-input" defaultChecked={ reviewRating === "Negative" } className="w-4 h-4 text-gray-600 bg-gray-100  border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 accent-gray-900 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                            <label className="w-full py-3 ml-2 text-sm font-bold text-gray-900 accent-gray-800 ">Negative</label>
                         </div>
                     </li>
                 </ul>
@@ -100,8 +95,8 @@ const ContentSettings = () => {
             <br />
             <br />
 
-            <label for="disabled-range" className="block mb-2 text-sm font-bold  text-white">Review Length</label>
-            <label for="disabled-range" className="block mb-2 text-sm font-medium  text-white">{reviewLength}</label>
+            <label className="block mb-2 text-sm font-bold  text-white">Review Length</label>
+            <label className="block mb-2 text-sm font-medium  text-white">{reviewLength}</label>
             <input id="minmax-range" type="range" min="0" max="100" value={reviewLength} onChange={handleSliderChange} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
 
             <br />
@@ -114,12 +109,9 @@ const ContentSettings = () => {
                 <span className="ml-3 text-sm font-medium dark:text-gray-300">Enable Custom Settings</span>
             </label>
 
-            <form  onSubmit={applyContentSettings}>
-                <button type="submit" className="px-2 py-1 font-bold text-gray-700  bg-white rounded hover:bg-gray-300"> Apply Setting </button>
-            </form>
+                <button onClick={applyContentSettings} type="submit" className="px-2 py-1 font-bold text-gray-700  bg-white rounded hover:bg-gray-300"> Apply Setting </button>
             </div>
 
-            
         </div>
     );
 }
--- frontEnd/src/SteamTag.js
+++ frontEnd/src/SteamTag.js
@@ -1,18 +1,19 @@
-const SteamTag = ({ tagName }) => {
+import { useState } from "react";
+const SteamTag = ({ tagName , onChange, checkCondition }) => {
 
     const [isChecked, setIsChecked] = useState(false);
 
-    const handleCheckboxChange = () => {
+    const handleCheckboxChange = (event) => {
         setIsChecked(!isChecked);
-        onChange(value, !isChecked); // Notify the parent component about the change.
+        onChange(event.target.value, !isChecked); // Notify the parent component about the change.
     };
 
     return (  
-        <div>
-            <input id="checkbox" type="checkbox" value={tagName} onChange={handleCheckboxChange} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-black-500  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 accent-green-800"/>
-            <label for="checkbox" className="ml-2 text-md font-bold text-gray-400 ">{ tagName }</label>
+        <div onChange={handleCheckboxChange}>
+            <input id="checkbox" type="checkbox" value={tagName} defaultChecked={ checkCondition.includes(tagName) } className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-black-500  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 accent-green-800"/>
+            <label className="ml-2 text-md font-bold text-gray-400 ">{ tagName }</label>
         </div>
                 
     );
 }
- export default SteamTag;
+export default SteamTag;
