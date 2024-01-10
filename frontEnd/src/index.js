1b73c8a67a3c5b258adf739e8b20da997fbde99d
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-01-09 21:29:43 -0500

    added user analytics

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { SettingsContextProvider } from './Contexts/SettingsContext';
+import { Analytics } from '@vercel/analytics/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <SettingsContextProvider>
         <App />
+        <Analytics />
     </SettingsContextProvider>
   </React.StrictMode>
 );
